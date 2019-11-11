75918d02f01f81757558e8534fe83b4a4edf693b
Manxi Lin <46046604+manxilin@users.noreply.github.com>
2019-11-11 19:42:45 +0100

    Update VMD_map.py

--- VMD_map.py
+++ VMD_map.py
@@ -3,9 +3,8 @@ import matplotlib.pyplot as plt
 import csv
 import h5py
 
-# 共处理两个数据集，MFPT与澳门大学
 global MCU_dir
-MCU_dir = 'F:\毕业设计\MCU\振动图\数据记录\\' # 澳门大学数据集保存路径
+#MCU_dir = ''
 def load_img():
     '''
     载入图片，只需要做一次
@@ -13,17 +12,16 @@ def load_img():
     '''
     # MCU
     dir = MCU_dir
-    model = My_CNN(64, 17, 1000, dir, channel=1)  # 初始化模型 图片尺寸40 共17类 每类20张图片
-    img_dir = 'F:\毕业设计\MCU\振动图\imgs\\'  # 图片保存路径
+    model = My_CNN(64, 17, 1000, dir, channel=1)  # 初始化模型
+    #img_dir = ''  # 图片保存路径
     model.read_img(img_dir)  # 初次使用需要载入图片
 
 def Lenet_MCU():
     '''
-            使用Lenet完整地训练澳门大学数据集，保存最优结果
             :return:
     '''
     dir = MCU_dir  # 数据集保存路径，之后就可以不用重新载入图片
-    model = My_CNN(64, 17, 1000, dir, channel=1)  # 初始化模型 图片尺寸40 共17类 每类1000张图片
+    model = My_CNN(64, 17, 1000, dir, channel=1)  # 初始化模型
     model.LeNet_5('Adam', lr=1e-3)
     hist_path = dir + 'LeNet_'
     path = dir + 'LeNet_model.h5'
@@ -31,11 +29,11 @@ def Lenet_MCU():
 
 def MCU_output():
     '''
-    输出澳门大学混淆矩阵
+    输出混淆矩阵
     :return:
     '''
     dir = MCU_dir
-    model = My_CNN(40, 17, 1000, dir, channel=1)  # 初始化模型 图片尺寸40 共16类 每类1000张图片
+    model = My_CNN(40, 17, 1000, dir, channel=1)  # 初始化模型
     model.LBCNN('Adam')
     path = dir + 'LeNet_model.h5'
     model.run_model(path)
