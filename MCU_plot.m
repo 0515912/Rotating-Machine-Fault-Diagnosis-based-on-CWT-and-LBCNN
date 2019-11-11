6652ac252e7223ad79555ed51f21e00c96c54867
Manxi Lin <46046604+manxilin@users.noreply.github.com>
2019-11-11 19:39:25 +0100

    Update MCU_plot.m

--- MCU_plot.m
+++ MCU_plot.m
@@ -1,12 +1,12 @@
 %%
 clear all; clc;
-load('F:\毕业设计\MCU\MCU_data.mat');
+%load('MCU_data.mat');
 wavename  = 'cmor3-3';
 size = 40;
 scal = 1024;
 frequency = 10240;
 time = 1;
-depress_pic_path = 'F:\毕业设计\MCU\时频图\imgs\';
+%depress_pic_path = 'imgs\';
 for i = 1:17000
         limit = 1000;
         d = Data(:,i);
